9764b3b602f1898c0fdad3f47a03c21c5d1503d3
Artsiom <sobolevskiy.a2010@mail.ru>
2024-09-17 21:45:29 +0300

    Правки API

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -21,8 +21,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && npm ci && npm run build',
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
